944b1dba37dc1fb08ae946bfb3a2b3dcaf3a9907
CHSRS <47526542+csrs2000@users.noreply.github.com>
2021-04-24 12:49:49 +0530

    Update add.py

--- Assignment-2/iir/add.py
+++ Assignment-2/iir/add.py
@@ -1,18 +1,17 @@
 import numpy as np
-#If using termux
-import subprocess
-import shlex
-#end if
 
-def add(X,Y):
-  x=len(X)
-  y=len(Y)
-  if x==y:
-    z= x+y
-  elif x>y:
-    y_pad=np.append(np.zeros(x-y),Y)
-    z=X+y_pad
-  else:
-    x_pad=np.append(np.zeros(y-x),X)
-    z=x_pad + Y
-  return z
+
+def add(x,y):
+    
+   
+    m = len(x)
+    n = len(y)
+    
+    if m == n:
+        z = x + y
+    elif m > n:
+        z = x + np.concatenate((np.zeros(m-n),y))
+    else:
+        z = np.concatenate((np.zeros(n-m),x)) + y
+        
+    return z
